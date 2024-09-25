7036947aea37ae3fd18a36e0c10ebebffb8fef3e
pawan <150579399+light607@users.noreply.github.com>
2024-09-25 13:00:09 +0530

    Update structs.rs

--- structs.rs
+++ structs.rs
@@ -2,7 +2,12 @@ struct Point{
     x:f64,
     y:f64
 }
- 
+ struct User{
+     username:String,
+     count_in:u64,
+     active:bool,
+     email:String
+ }
  imp Point{
     fn distance(&self,other:&Point)->f64{
         let dx = self.x-other.x;
@@ -11,16 +16,16 @@ let dy=self.y-other.y;
     
     }
  }
-
+trait Summary{
+    fn origin(&self)->String
+}
  imp Point{
 
     fn origin()->Point{
         Point {x:0.0,y:0.0}
     }
  }
-
-
- struct Color(u8,u8,u8);
+     struct Color(u8,u8,u8);
  let black =Color(0,0,0,);
 
 imp Point {
@@ -42,3 +47,24 @@ imp Point{
  }
 struct Color(u8,u8,u8);
 let black =Color(0,0,0);
+
+fn main(){
+    let user =User{
+        username:"pawan",
+        active:true,
+        count_in:5,
+        email:"pawanpediredla@gmail.com"
+    }
+        let user2=User{
+        email:String::from("pawanpediredla@gmail.com"),
+        ...user1
+        }
+    println!("{}",user.username);
+Struct Color(u8,u8,u8);
+let black =Color(255,255,255);
+}
+imp Summary for User {
+    fn origin(&self)->String{
+    format!("{} ,({})", self.username,self.email);
+    }
+}
